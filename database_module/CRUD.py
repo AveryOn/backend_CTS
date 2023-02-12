cda0eec1742214506cd9fc803e44b22bc2937ab9
AveryOn <lnkonion2@gmail.com>
2023-02-11 16:52:19 -0900

    backend commit

--- database_module/CRUD.py
+++ database_module/CRUD.py
@@ -1,14 +1,24 @@
 ####################################################################################################
 #         МОДУЛЬ ВЗАИМОДЕЙСТВИЯ С БД. ПОЛУЧЕНИЕ /ОБНОВЛЕНИЕ /УДАЛЕНИЕ /СОЗДАНИЕ ДАННЫХ В БД        #
 ####################################################################################################
- 
+
 # Импорт ORM-таблиц
 from models_user import User, UserCart, UserChat, Message
 from models_product import Product, Comment
 
 # Импорт Pydantic-моделей
-
+from schemas_module import user, user_cart, user_chat, product, message, comment
 
 # Импорт тулов с sqlalchemy
 from sqlalchemy.orm import Session
 from sqlalchemy import select, update
+
+
+#===========>>>   ВЗАИМОДЕЙСТВИЕ С БАЗОЙ ДАННЫХ  -  USERS.db   <<<==================
+
+# Создание пользователя и корзины товаров
+
+def create_user(db: Session, user: user.UserCreate):
+    pass
+
+
---
+++ schemas_module/user.py
@@ -0,0 +1,31 @@
+####################################################################################################
+#                                 МОДЕЛЬ PYDANTIC ДЛЯ ПОЛЬЗОВАТЕЛЯ                                 #
+####################################################################################################
+
+from pydantic import BaseModel
+
+# Модель нигде не используется играет роль фундамента для обьекта user
+class UserBase(BaseModel):
+    pass
+
+# Модель для создания нового пользователя
+class UserCreate(UserBase):
+    email: str
+    username: str
+    password: str
+    name: str | None=None
+    lastname: str | None=None
+
+# Модель для чтения пользователя из БД / Возврата на клиент
+class User(UserBase):
+    id: int
+    username: str
+    email: str
+    name: str | None=None
+    lastname: str | None=None
+    image: str | None=None
+    sex: str | None=None
+    is_active: bool | None=None
+
+    class Config:
+        orm_mode = True
