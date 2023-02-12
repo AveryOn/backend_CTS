8378c4878282206fa71fc0bccf668a6094db59e1
AveryOn <lnkonion2@gmail.com>
2023-02-11 15:54:48 -0900

    backend commit

--- database_module/CRUD.py
+++ database_module/CRUD.py
@@ -2,3 +2,13 @@
 #         МОДУЛЬ ВЗАИМОДЕЙСТВИЯ С БД. ПОЛУЧЕНИЕ /ОБНОВЛЕНИЕ /УДАЛЕНИЕ /СОЗДАНИЕ ДАННЫХ В БД        #
 ####################################################################################################
 
+# Импорт ORM-таблиц
+from models_user import User, UserCart, UserChat, Message
+from models_product import Product, Comment
+
+# Импорт Pydantic-моделей
+
+
+# Импорт тулов с sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import select, update
